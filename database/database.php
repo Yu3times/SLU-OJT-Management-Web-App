1059f62f206418427b1e468d047669672cdfb0dc
JobertD <2226374@slu.edu.ph>
2023-11-27 22:49:09 +0800

    Add constructors and Organize files

--- database/database.php
+++ database/database.php
@@ -4,17 +4,17 @@ class DB {
     private $dbUsername = "root";
     private $dbPassword = "";
     private $dbName = "ojt_monitoring";
+    private $db;
 
     public function __construct(){
         if(!isset($this -> db)){
             $connect = new mysqli($this -> dbHost, $this -> dbUsername, $this -> dbPassword, $this -> dbName);
             if($connect -> connect_error){
-                die("Failed to connect with mySQL: " . $connect -> connect_error)
+                die("Failed to connect with mySQL: " . $connect -> connect_error);
             } else{
                 $this -> db = $connect;
             }
         }
     }
 }
-
 ?>
