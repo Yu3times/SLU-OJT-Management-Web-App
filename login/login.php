44ebe4215024df5b0ce6049e75c676887b1ba9d3
Yustine Arevalo <2225602@slu.edu.ph>
2023-12-05 20:58:58 +0800

    Fix reference to user table in database

--- login/login.php
+++ login/login.php
@@ -1,13 +1,13 @@
 <?php
-  require("database/database.php");
-  include "database/constructors/user.php";
+  require("../database/database.php");
+  include "../database/constructors/user.php";
   session_start();
 
   if (isset($_POST['userID'])){
     $userId = $_POST['userID'];
     $password = $_POST['password'];
 
-    $statement = $db->prepare("SELECT * FROM USER WHERE userId = ? AND password = ?");
+    $statement = $db->prepare("SELECT * FROM user WHERE userId = ? AND password = ?");
     $statement->bind_param('ss', $userId, $password);
     $statement->execute();
     $result = $statement->get_result();
