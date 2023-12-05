edb3a1b32b0248e96b1fe4a01a4e953ccab8e6e1
JobertD <2226374@slu.edu.ph>
2023-12-05 20:21:55 +0800

    reorganize for consistency

--- login/index.php
+++ login/index.php
@@ -4,8 +4,8 @@
     if (!isset($_SESSION['userID'])){
         include "loginPage.html";
     } else{
-        if (isset($_SESSION['isStudent'])) {
-            include "student/index.html"
+        if ($_SESSION['isStudent']) {
+            include "student/index.html";
         } else {
             header("Location: admin/index.html");
         }
--- login/login.php
+++ login/login.php
@@ -7,21 +7,23 @@
     $userId = $_POST['userID'];
     $password = $_POST['password'];
 
-    $statement = $db -> prepare("SELECT * FROM USER WHERE userId = ? AND password = ?");
-    $statement -> bind_param('ss', $userId, $password);
-    $statement -> execute();
-    $result = $statement -> get_result();
+    $statement = $db->prepare("SELECT * FROM USER WHERE userId = ? AND password = ?");
+    $statement->bind_param('ss', $userId, $password);
+    $statement->execute();
+    $result = $statement->get_result();
 
-    if ($result -> num_rows != 0){
-      $_SESSION['userID'] = $userId;
-      $data = $result -> fetch_assoc();
-      if ($data['type'] = 1){
+    if ($result->num_rows != 0){
+      $user = $result->fetch_assoc();
+      $_SESSION['userID'] = $user['userId'];
+
+      if ($result['type'] == 1){
         $_SESSION['isStudent'] = true;
       }
     }
 
-    $statement -> close();
+    $statement->close();
   }
+  
   header("Location: index.php");
   exit();
 ?>
