794a8a7704b0b904eedf361f0e98f4dc0ef4d819
Yustine <2225602@slu.edu.ph>
2023-12-05 13:47:33 +0800

    Change action reference to login.php in loginPage

--- login/index.php
+++ login/index.php
@@ -4,6 +4,11 @@
     if (!isset($_SESSION['userID'])){
         include "loginPage.html";
     } else{
+        /*
+        if (isset($_SESSION['isStudent'])) {
+            
+        }
+        */
         include "test.html";
     }
 ?>
