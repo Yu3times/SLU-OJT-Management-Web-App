52ad4b140c61aa5821988e5c93b6380becb6202e
JobertD <2226374@slu.edu.ph>
2023-12-07 23:30:23 +0800

    Add nodeJS files

---
+++ teacher/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+var conn = mysql.createConnection({
+    host:'localhost', user:'root', password:'', database:'ojt_monitoring'
+});
+
+const app = express();
+app.use(express.static('public'));
+app.set('views', `${__dirname}/view`);
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(session({secret: 'frogrammers',
+                 resave: true, 
+                 saveUninitialized: true
+                }));
+
+app.listen(8001, 'localhost');
+
+app.post('/login', (req, res) => {
+    req.session.userID = req.body.userID;
+    req.session.fullName = 'John Doe'; 
+    res.json(userID);
+});
+
+app.render('index', (err, html)=>{
+    if (err){
+        console.log(err);
+    } else{
+        console.log(html);
+    }
+})
+
