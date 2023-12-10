bb002839581f308ce8faa1f71f3abe610735ab2a
JobertD <2226374@slu.edu.ph>
2023-12-10 13:33:12 +0800

    merge edits

--- teacher/routes.js
+++ teacher/routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const mysql = require('mysql');
 
 var db = mysql.createConnection({
-    host:'localhost', user:'root', password:'', database:'ojt_monitoring'
+    host: 'localhost', user: 'root', password: '', database: 'ojt_monitoring'
 });
 
 router.use(session({
@@ -18,48 +18,40 @@ const noCacheMiddleware = (req, res, next) => {
     res.header('Expires', '-1');
     res.header('Pragma', 'no-cache');
     next();
-  };
+};
 
 router.use(noCacheMiddleware);
 
 const requireLogin = (req, res, next) => {
-    if(!req.session.userId){
+    if (!req.session.userId) {
         return res.send('<script>window.location.href="/login/loginPage.php";</script>');
     }
     next();
 };
 
-/*
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
-  });
-*/
-
 router.get('/redirect', (req, res) => {
     res.render('redirect');
-  });
+});
 
 router.post('/homepage', (req, res) => {
     const userId = req.body.userID;
-    
-    req.session.userID = req.body.userID;
-    const statement = db.prepare("SELECT firstName, lastName FROM student WHERE userId = ?");
-    
+
+    const statement = db.prepare("SELECT firstName, lastName FROM teacher WHERE userId = ?");
+
     statement.bind_param("i", userId);
     statement.execute();
     const result = statement.get_result();
-    
-    if (result.num_rows > 0) {
+
+    if (result.num_rows != 0) {
         const row = result.fetch_assoc();
         const fullName = row.firstName + ' ' + row.lastName;
         console.log('FullName:', fullName);
-        res.render('homepage', {fullName: fullName});
+        res.render('homepage', { fullName: fullName });
     } else {
         res.render('homepage', { fullName: "No data found" });
     }
-    
+
     statement.close();
 });
-
 
 module.exports = router;
